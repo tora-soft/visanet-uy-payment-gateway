ded24d3d2e6e53459073a8932d0bcde054f5d3ca
Federico Giust <fgiustdj@gmail.com>
2014-08-19 19:23:21 -0300

    bug fixes

--- visanet-uy-payment-gateway.php
+++ visanet-uy-payment-gateway.php
@@ -259,7 +259,10 @@ function woocommerce_visanet_init(){
 			setcookie("woocommerce_order_key", $return_url['query'], time()+3600, "/wordpress/", "www.tora-soft.com");
 			setcookie("woocommerce_order_returnurl", $order->get_checkout_order_received_url(), time()+3600, "/wordpress/", "www.tora-soft.com");
 
-	        $txnid = 'LS' . $order_id . date("ymd");
+			$suffix_order_id = uniqid(rand(10,1000),false);
+			$suffix_order_id = substr($suffix_order_id,rand(0,strlen($suffix_order_id) - 6),6);
+
+	        $txnid = 'LS' . $order_id . $suffix_order_id;
 	  
 	 		$array_send = $this->get_array_send( $order, $txnid );
 
@@ -373,9 +376,9 @@ function woocommerce_visanet_init(){
 							$order->update_status( 'failed',  __( 'Error: ' . $resultadoAutorizacion . ' | ' . $arrayOut['errorCode'] . ' - ' . $arrayOut['errorMessage'], 'woocommerce' ) );
 						}
 						if ($resultadoAutorizacion == '05'){
-							$order->update_status( 'failed',  __( 'Error: ' . $resultadoAutorizacion . ' | ' . $arrayOut['errorCode'] . ' - ' . $arrayOut['errorMessage'], 'woocommerce' ) );
+							$order->update_status( 'cancelled',  __( 'Error: ' . $resultadoAutorizacion . ' | ' . $arrayOut['errorCode'] . ' - ' . $arrayOut['errorMessage'], 'woocommerce' ) );
 						}
-						$result = 'failed';
+						$result = 'cancelled';
 						$this->web_redirect($order->get_checkout_order_received_url());
 
 			
