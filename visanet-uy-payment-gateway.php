e885a3665b51c2007009d050392d2151249f50d1
Federico Giust <fgiustdj@gmail.com>
2014-08-21 14:33:26 -0300

    custom fields

--- visanet-uy-payment-gateway.php
+++ visanet-uy-payment-gateway.php
@@ -58,7 +58,7 @@ function woocommerce_visanet_init(){
 			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id 	, array( $this, 'process_admin_options' ));
             add_action( 'woocommerce_thankyou_' . $this->id							, array( $this, 'check_response'  ));
 
-			add_action( 'woocommerce_admin_order_data_after_billing_address', 'display_custom_field', 10, 1 );
+			add_action( 'woocommerce_admin_order_data_after_billing_address', 'display_custom_field');
 
 			if ( ! $this->is_valid_for_use() ) {
 				$this->enabled = false;
